5171aca6e935c5b40df4275bf3037805738c663e
Karina Pichardo <karinapichardo@Karinas-MacBook-Air.local>
2019-11-25 23:17:38 -0500

    third commit

---
+++ concert.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Concert {
+
+    public static void main(String[] args) {
+
+        //creating objects of class ticket
+        ticket generalAdmission = new ticket(200.50, 8, 16.04);
+        ticket vip = new ticket(400.50, 8, 32.04);
+        ticket reservedSeating = new ticket(600.50, 8, 48.04);
+
+
+        System.out.println("Listing of All Packages (packages include 8 tickets)");
+
+// calling method
+        System.out.println("\nGeneral Admission Package - Total Charges: $" + generalAdmission.calculateTotal());
+
+        System.out.println("VIP Package - Total Charges: $" + vip.calculateTotal());
+
+        System.out.println("Reserved Seating Package - Total Charges: $" + reservedSeating.calculateTotal());
+
+    }
+}
+
+class ticket {
+    double price;                //price of each ticket
+    int quantity;               //number of tickets
+    double taxAmount;          //tax imposed
+
+    //constructor
+    ticket(double p, int q, double ta) {
+        price = p;
+        quantity = q;
+        taxAmount = ta;
+    }
+
+    // method to calculate total charges
+    double calculateTotal() {
+        double total;
+        total = (price * quantity) + taxAmount;
+        return total;
+
+    }
+}
