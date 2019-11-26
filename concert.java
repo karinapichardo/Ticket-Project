06942f88b2542686d3a2d846d1af116d76a26013
Karina Pichardo <58063163+karinapichardo@users.noreply.github.com>
2019-11-26 13:11:32 -0500

    Update concert.java

--- concert.java
+++ concert.java
@@ -5,9 +5,9 @@ public class Concert {
     public static void main(String[] args) {
 
         //creating objects of class ticket
-        ticket generalAdmission = new ticket(200.50, 8, 16.04);
-        ticket vip = new ticket(400.50, 8, 32.04);
-        ticket reservedSeating = new ticket(600.50, 8, 48.04);
+        Ticket generalAdmission = new Ticket(200.50, 8, 16.04);
+        Ticket vip = new Ticket(400.50, 8, 32.04);
+        Ticket reservedSeating = new Ticket(600.50, 8, 48.04);
 
         System.out.println("Listing of All Packages (packages include 8 tickets)");
 
@@ -20,13 +20,13 @@ public class Concert {
     }
 }
 
-class ticket {
+class Ticket {
     double price;                //price of each ticket
     int quantity;               //number of tickets
     double taxAmount;          //tax imposed
 
     //constructor
-    ticket(double price, int quantity, double taxAmount) {
+    Ticket(double price, int quantity, double taxAmount) {
         this.price = price;
         this.quantity = quantity;
         this.taxAmount = taxAmount;
